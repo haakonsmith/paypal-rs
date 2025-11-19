2327a38e0ccd1c714c1a19ba130e05f1c77c2b90
Haakon Smith <haakonss04@gmail.com>
2025-11-19 13:47:09 +0100

    Update error handling to handle paypals special errors....

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,43 @@
 //! Errors created by this crate.
 use crate::data::common::LinkDescription;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
+
+/// This comes from the ass backwards reality that is paypal rest api.
+/// Basically they lie and say they will return a vec but instead send back a single item
+///
+/// This impls Default, the default implementation returns a Many(Vec::new()).
+/// Which seems like a reasonable representation but :shrug:
+///
+/// Ultimately you probably wanna interface with this using the to_vec() method
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(bound = "T: Serialize + DeserializeOwned")]
+#[serde(untagged)]
+pub enum OneOrMany<T: DeserializeOwned> {
+    /// A single value was returned from the api
+    One(T),
+    /// None or Many values were returned.
+    Many(Vec<T>),
+}
+
+impl<T: DeserializeOwned + Clone> OneOrMany<T> {
+    /// Flattens the types into a Vec representation. Will clone
+    pub fn to_vec(&self) -> Vec<T> {
+        match self {
+            OneOrMany::One(x) => vec![x.clone()],
+            OneOrMany::Many(items) => items.clone(),
+        }
+    }
+}
+
+impl<T: DeserializeOwned> Default for OneOrMany<T> {
+    fn default() -> Self {
+        Self::Many(vec![])
+    }
+}
 
 /// A paypal api response error.
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,12 +49,14 @@ pub struct PaypalError {
     /// Paypal debug id
     pub debug_id: Option<String>,
     /// Error details
-    pub details: Vec<HashMap<String, String>>,
+    #[serde(default)]
+    pub details: OneOrMany<HashMap<String, String>>,
     /// Only available on Identity errors
     pub error: Option<String>,
     /// Only available on Identity errors
     pub error_description: Option<String>,
     /// Links with more information about the error.
+    #[serde(default)]
     pub links: Vec<LinkDescription>,
 }
 
@@ -96,3 +132,51 @@ impl fmt::Display for InvalidCountryError {
 }
 
 impl Error for InvalidCountryError {}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_decoding() {
+        serde_json::from_value::<PaypalError>(json!({
+                  "name":"INVALID_REQUEST",
+                  "message":"Request is not well-formed, syntactically incorrect, or violates schema.",
+                  "debug_id":"f896367ed3b42",
+                  "information_link":"",
+                  "details":{
+                    "issue":" INVALID_PARAMETER_SYNTAX",
+                    "description":"api_integration_preference is mandatory when operation is API_INTEGRATION.",
+                    "field":"/operations/0",
+                    "location":"body"
+                  },
+                  "links":[],
+                  "sys":{
+                    "links":{
+                      "jsBaseUrl":"https://api-m.sandbox.paypal.com/js",
+                      "cssBaseUrl":"https://api-m.sandbox.paypal.com/css",
+                      "templateBaseUrl":"https://api-m.sandbox.paypal.com/templates",
+                      "resourceBaseUrl":"https://api-m.sandbox.paypal.com"},
+                    "pageInfo":{
+                      "date":"Nov 19, 2025 02:30:08 -0 8:00",
+                      "hostName":"rZJvnqaaQhLn/nmWT8cSUjOx898qoYZ0pO7iw8eqgZjo3CyMhqQpbsz9WA4gBCrqQHOe7b4a cg4",
+                      "rlogId":"rZJvnqaaQhLn%2FnmWT8cSUvl%2BhVU1VdNAVDKJ5S9HXX%2Bw%2FCbJ%2BGKaXb5ubGp1hdQRK s4ug1gnxWPOd5unF8U%2FcWo%2FPSWe6ohg1RR9i45iMUY_19a9baa1b9a",
+                      "script":"node",
+                      "debug":null
+                    },
+                    "tracking":{
+                      "fpti":{
+                        "dataString":"pgrp=partnselleronbnodeserv%2F.dust&page=partnselleronbnodeserv%2F.dust&qual=&comp=partnselleronbnodeserv&tsrce=partnselleronbnodeserv&cu=0&ef_policy=&c_prefs=&pxpguid=&pgst=1763548208026&calc=f896367ed3b42&csci=f1fd1839da674009ac962140c62b2747&nsid=&pgtf=Nodejs&s=ci&env=sandbox",
+                        "varName":"pta",
+                        "name":"pta",
+                        "jsURL":"https://www.paypalobjects.com",
+                        "serverURL":"https://t.paypal.com/ts"
+                      }
+                    }
+                  }
+                }
+        )).unwrap();
+    }
+}
