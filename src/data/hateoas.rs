a8b3a654ae5a1cec3f0389d19800c057f243277c
Haakon Smith <haakonss04@gmail.com>
2025-12-02 15:06:54 +0100

    Fix hateoas

--- src/data/hateoas.rs
+++ src/data/hateoas.rs
@@ -3,7 +3,7 @@
 use crate::data::common::LinkDescription;
 
 /// This is a extension trait for Vec<LinkDescription> to make query Hateoas easier
-pub trait HateoasExt {
+pub trait HateoasExt<'a> {
     /// Retrieves a link from the collection by its `rel` attribute.
     ///
     /// # Arguments
@@ -13,46 +13,24 @@ pub trait HateoasExt {
     /// # Returns
     ///
     /// Returns `Some(href)` if a link with the matching `rel` is found, `None` otherwise.
-    fn get_link(&mut self, rel: impl AsRef<str>) -> Option<String>;
+    fn get_link(&'a mut self, rel: impl AsRef<str>) -> Option<&'a LinkDescription>;
 
     /// Gets the link with rel == 'self'
-    fn get_self(&mut self) -> Option<String> {
+    fn get_self(&'a mut self) -> Option<&'a LinkDescription> {
         self.get_link("self")
     }
 
     /// Gets the link with rel == 'up'
-    fn get_up(&mut self) -> Option<String> {
+    fn get_up(&'a mut self) -> Option<&'a LinkDescription> {
         self.get_link("up")
     }
 }
 
-impl<'a, T: Iterator<Item = &'a LinkDescription>> HateoasExt for T {
-    fn get_link(&mut self, rel: impl AsRef<str>) -> Option<String> {
+impl<'a, T: Iterator<Item = &'a LinkDescription>> HateoasExt<'a> for T {
+    fn get_link(&'a mut self, rel: impl AsRef<str>) -> Option<&'a LinkDescription> {
         let rel: &str = rel.as_ref();
         let link = self.find(|l| l.rel.as_deref() == Some(rel));
 
-        link.map(|e| e.href.clone())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_finding_links() {
-        let links = vec![LinkDescription {
-            rel: Some("test".into()),
-            ..Default::default()
-        }];
-
-        assert_eq!(links.iter().get_link("test"), Some("".into()));
-
-        let links = vec![LinkDescription {
-            rel: Some("up".into()),
-            ..Default::default()
-        }];
-
-        assert_eq!(links.iter().get_up(), Some("".into()));
+        link
     }
 }
